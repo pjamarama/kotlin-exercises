8c8639e8e6a61fdb6e992de2eaeb8a288e01c31b
Alexey <lukomorie2001@gmail.com>
2021-09-14 12:00:08 +0700

    Mumbling exercise added

---
+++ src/main/kotlin/codewars/Mumbling.kt
@@ -0,0 +1,27 @@
+/*
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+*/
+
+package codewars
+
+class Mumbling {
+}
+
+fun main() {
+    println(accum("cwAt"))
+}
+
+fun accum(sounds: String): String {
+//    val soundsList = sounds.toList()
+//    val soundsListIterator = soundsList.iterator()
+//
+//    while (soundsListIterator.hasNext()) {
+//        soundsListIterator.toLowerCase()
+//    }
+//
+//
+//    return result
+    return "haha"
+}
