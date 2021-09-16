e79f9eb960f397992c34832245b7e9a4d927fbf9
Alexey <lukomorie2001@gmail.com>
2021-09-16 17:18:17 +0700

    OppositesAttract added

---
+++ src/main/kotlin/codewars/OppositesAttract.kt
@@ -0,0 +1,13 @@
+package codewars
+
+/*
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each.
+If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+Write a function that will take the number of petals of each flower and return true if they are in love and false
+if they aren't.
+ */
+class OppositesAttract {
+
+    fun loveFun(flowerA: Int, flowerB: Int): Boolean = (flowerA + flowerB) % 2 != 0
+
+}
